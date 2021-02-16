667d67f5353603a074e4d582f8324b79add029bf
amolbote <amol.a.bote@gmail.com>
2021-02-16 23:38:21 +0530

    Sorting data based on course category

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,16 @@ import { noop, Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { createHttpObservable } from "./utils/util";
 
+interface Courses {
+  category: string;
+  courseListIcon: string;
+  description: string;
+  iconUrl: string;
+  id: number;
+  lessonsCount: number;
+  longDescription: string;
+}
+
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
@@ -11,17 +21,24 @@ import { createHttpObservable } from "./utils/util";
 export class AppComponent implements OnInit {
   url: string = "https://my-json-server.typicode.com/amolbote/jsondata/courses";
 
-  http$: Observable<any>;
-  courses$: Observable<any>;
+  http$: Observable<Courses[]>;
+  beginnerCourses$: Observable<Courses[]>;
+  advancedCourses$: Observable<Courses[]>;
+  courses$: Observable<Courses[]>;
   ngOnInit(): void {
     this.http$ = createHttpObservable(this.url);
 
     //this.http$.subscribe(data => console.log(data));
     this.courses$ = this.http$.pipe(map(courses => Object.values(courses)));
-    this.courses$.subscribe(
-        data => console.log(data),
-        noop,
-        () => console.log('completed')
-      );
+    this.courses$.subscribe(data => console.log("!!!", data), noop, () =>
+      console.log("completed")
+    );
+
+    this.beginnerCourses$ = this.courses$.pipe(
+      map(courses => courses.filter(course => course.category === "BEGINNER"))
+    );
+    this.advancedCourses$ = this.courses$.pipe(
+      map(courses => courses.filter(course => course.category === "ADVANCED"))
+    );
   }
 }
