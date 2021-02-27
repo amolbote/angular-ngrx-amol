a3658911270c7fec28114e623e556620ab2568fc
amolbote <amol.a.bote@gmail.com>
2021-02-27 22:19:41 +0530

    merge operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,7 @@
 import { Component, OnInit, VERSION } from "@angular/core";
-import { noop, Observable } from "rxjs";
-import { map, shareReplay } from "rxjs/operators";
-import { createHttpObservable } from "./utils/util";
 
-interface Courses {
-  category: string;
-  courseListIcon: string;
-  description: string;
-  iconUrl: string;
-  id: number;
-  lessonsCount: number;
-  longDescription: string;
-}
+import { merge } from "rxjs/operators";
+import { interval } from "rxjs";
 
 @Component({
   selector: "my-app",
@@ -19,27 +9,10 @@ interface Courses {
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  url: string = "https://my-json-server.typicode.com/amolbote/jsondata/courses";
-
-  http$: Observable<Courses[]>;
-  beginnerCourses$: Observable<Courses[]>;
-  advancedCourses$: Observable<Courses[]>;
-  courses$: Observable<Courses[]>;
   ngOnInit(): void {
-    this.http$ = createHttpObservable(this.url);
-
-    // shareReplay operator will make only one http call and
-    // shares result with all subscribers (check network tab)
-    this.courses$ = this.http$.pipe(
-      map(courses => Object.values(courses)),
-      shareReplay()
-    );
-
-    this.beginnerCourses$ = this.courses$.pipe(
-      map(courses => courses.filter(course => course.category === "BEGINNER"))
-    );
-    this.advancedCourses$ = this.courses$.pipe(
-      map(courses => courses.filter(course => course.category === "ADVANCED"))
-    );
+    const first = interval(2500);
+    const second = interval(1000);
+    const example = first.pipe(merge(second));
+    example.subscribe(console.log);
   }
 }
