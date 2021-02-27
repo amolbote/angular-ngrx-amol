a9a667910c88f39ba5d301f101da284e8fc09370
amolbote <amol.a.bote@gmail.com>
2021-02-27 23:51:47 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit, VERSION } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  VERSION,
+  ViewChild
+} from "@angular/core";
 
-import { merge } from "rxjs/operators";
-import { interval } from "rxjs";
+import { exhaustMap, merge, take } from "rxjs/operators";
+import { fromEvent, interval } from "rxjs";
 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    const first = interval(2500);
-    const second = interval(1000);
-    const example = first.pipe(merge(second));
-    example.subscribe(console.log);
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild("mySelect") mySelect: ElementRef;
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    fromEvent(document, "scroll").subscribe(val =>
+      console.log(this.mySelect.nativeElement.value)
+    );
   }
 }
